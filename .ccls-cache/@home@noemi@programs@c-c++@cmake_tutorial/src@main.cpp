d8c322741f34bce972764dfa007fe1213a622a4f
trapano-monogamo <filippo.cozzi.fc@gmail.com>
2022-05-12 23:14:36 +0200

    .

---
+++ .ccls-cache/@home@noemi@programs@c-c++@cmake_tutorial/src@main.cpp
@@ -0,0 +1,10 @@
+#include "include/vec.hpp"
+#include <iostream>
+
+int main()
+{
+	 std::cout << "Hello, World!" << std::endl;
+	 std::cout << Vec::dist(Vec(0,0), Vec(0,0)) << std::endl;
+
+	 return 0;
+}
--- .ccls-cache/@home@noemi@programs@c-c++@cmake_tutorial/src@vec.cpp
+++ .ccls-cache/@home@noemi@programs@c-c++@cmake_tutorial/src@vec.cpp
@@ -1,5 +1,6 @@
 #include "include/vec.hpp"
+#include <cmath>
 
 static float Vec::dist(Vec v0, Vec v1) {
-	 return 3.0f;
+	 return sqrt((v0.x - v1.x) * (v0.x - v1.x) - (v0.y - v1.y) * (v0.y - v1.y));
 }
